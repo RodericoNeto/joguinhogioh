fae553890b887f72ca8235674f3d29ae2e84de3f
rodericoneto <rodericocrneto@gmail.com>
2023-03-28 18:07:55 -0300

    adicionei os 12 quadrados

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 
 import './App.css';
 import audioNoel from './assets/audios/audio_papainoel01.mp3'
-import imgNoel from './assets/imagens/papainoel1.jpg'
+import imgNoel from './assets/imagens/papainoel01gif.gif'
 import audioGato from './assets/audios/audio_gato01.mp3'
-import imgGato from './assets/imagens/gato01.png'
+import imgGato from './assets/imagens/gato01gif.gif'
 import audioPiscina from './assets/audios/audio_piscina01.mp3'
-import imgPiscina from './assets/imagens/piscina01.png'
+import imgPiscina from './assets/imagens/piscina01gif.gif'
 import audioCachorro from './assets/audios/audio_cachorro01.mp3'
-import imgCachorro from './assets/imagens/cachorro01.jpg'
+import imgCachorro from './assets/imagens/cachorro01gif.gif'
 import audioLua from './assets/audios/audio_lua01.mp3'
 import imgLua from './assets/imagens/lua01gif.gif'
-import audioBanana from './assets/audios/audio_banana01.mp3'
-import imgBanana from './assets/imagens/banana01.jpg'
+import audioBanana from './assets/audios/audio_banana02.mp3'
+import imgBanana from './assets/imagens/banana01gif.gif'
+
+import audioBola from './assets/audios/audio_bola02.mp3'
+import imgBola from './assets/imagens/bola01gif.gif'
+
+import audioSapo from './assets/audios/audio_sapo01.mp3'
+import imgSapo from './assets/imagens/sapo01gif.gif'
+
+import audioPeixe from './assets/audios/audio_peixinho01.mp3'
+import imgPeixe from './assets/imagens/peixe01gif.gif'
+
+import audioCelular from './assets/audios/audio_celular01.mp3'
+import imgCelular from './assets/imagens/celular01gif.gif'
+
+import audioPinto from './assets/audios/audio_pintinho01.mp3'
+import imgPinto from './assets/imagens/pintinho01gif.gif'
+
+// import imgPassaro from './assets/imagens/passarinho01gif.gif'
+
+import audioTitio from './assets/audios/audio_titio01.mp3'
+import imgTitio from './assets/imagens/titio01.jpeg'
+
+
 
 import CardButton from './components/CardButton';
 
@@ -46,28 +68,28 @@ function App() {
         img={imgBanana}
         />
         <CardButton
-        audio={audioNoel}
-        img={imgNoel}
+        audio={audioBola}
+        img={imgBola}
         />
         <CardButton
-        audio={audioNoel}
-        img={imgNoel}
+        audio={audioPinto}
+        img={imgPinto}
         />
         <CardButton
-        audio={audioNoel}
-        img={imgNoel}
-        />
-                <CardButton
-        audio={audioNoel}
-        img={imgNoel}
+        audio={audioPeixe}
+        img={imgPeixe}
         />
         <CardButton
-        audio={audioNoel}
-        img={imgNoel}
+        audio={audioCelular}
+        img={imgCelular}
         />
         <CardButton
-        audio={audioNoel}
-        img={imgNoel}
+        audio={audioTitio}
+        img={imgTitio}
+        />
+        <CardButton
+        audio={audioSapo}
+        img={imgSapo}
         />
       </div>
     </div>

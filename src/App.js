602d36e050c30833e088f427690b454d0a9a17da
rodericoneto <rodericocrneto@gmail.com>
2023-03-28 21:34:03 -0300

    adicionando o loading

--- src/App.js
+++ src/App.js
@@ -12,36 +12,42 @@ import audioLua from './assets/audios/audio_lua01.mp3'
 import imgLua from './assets/imagens/lua01gif.gif'
 import audioBanana from './assets/audios/audio_banana02.mp3'
 import imgBanana from './assets/imagens/banana01gif.gif'
-
 import audioBola from './assets/audios/audio_bola02.mp3'
 import imgBola from './assets/imagens/bola01gif.gif'
-
 import audioSapo from './assets/audios/audio_sapo01.mp3'
 import imgSapo from './assets/imagens/sapo01gif.gif'
-
 import audioPeixe from './assets/audios/audio_peixinho01.mp3'
 import imgPeixe from './assets/imagens/peixe01gif.gif'
-
 import audioCelular from './assets/audios/audio_celular01.mp3'
 import imgCelular from './assets/imagens/celular01gif.gif'
-
 import audioPinto from './assets/audios/audio_pintinho01.mp3'
 import imgPinto from './assets/imagens/pintinho01gif.gif'
-
-// import imgPassaro from './assets/imagens/passarinho01gif.gif'
-
 import audioTitio from './assets/audios/audio_titio01.mp3'
 import imgTitio from './assets/imagens/titio01.jpeg'
 
-
+// imports react
 
 import CardButton from './components/CardButton';
+import Loading from './components/Loading/Loading';
+import { useState, useEffect } from 'react';
+
+
 
 
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    window.addEventListener('load', ()=>{
+      setLoading(false)
+    })
+  }, [])
+
+
   return (
     <div className="App">
+      {loading ? <Loading/> : null}
       <div className='cardsContainerGrid'>
         <CardButton
         audio={audioNoel}
